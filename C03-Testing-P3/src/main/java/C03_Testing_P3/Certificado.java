ef68c761353676ad133df17b5bd59fd0d0aabb2a
Oscar Veloso Delgado <91962889+OscarVeloso5@users.noreply.github.com>
2021-12-21 14:29:07 +0100

    Add files via upload

--- C03-Testing-P3/src/main/java/C03_Testing_P3/Certificado.java
+++ C03-Testing-P3/src/main/java/C03_Testing_P3/Certificado.java
@@ -3,73 +3,74 @@ package C03_Testing_P3;
 public class Certificado{
 
 	int certificadoFinal;
-	
+	mantenibilidad mantenibilidad;
+	Adecuacion_Funcional adecuacionFun;
 
-	public Certificado(int adecuacionFun, int mantenibiliad, int certificadoFinal) {
+	public Certificado(Adecuacion_Funcional adecuacionFun, mantenibilidad mantenibilidad, int certificadoFinal) {
 		this.adecuacionFun = adecuacionFun;
-		this.mantenibiliad = mantenibiliad;
+		this.mantenibilidad = mantenibilidad;
 		this.certificadoFinal = certificadoFinal;
 	}
 	public String certificadofinal() {
 		String msgCertificado= "";
-		if (adecuacionFun == 1) {
-			if (mantenibilidad ==1) {
+		if (adecuacionFun.adecuacionFun == 1) {
+			if (mantenibilidad.mantenibilidad ==1) {
 				certificadoFinal = 1;
-			}else if (mantenibilidad ==2) {
+			}else if (mantenibilidad.mantenibilidad ==2) {
 				certificadoFinal = 1;
-			}else if (mantenibilidad ==3) {
+			}else if (mantenibilidad.mantenibilidad ==3) {
 				certificadoFinal = 1;
-			}else if (mantenibilidad ==4) {
+			}else if (mantenibilidad.mantenibilidad ==4) {
 				certificadoFinal = 1;
-			}else if (mantenibilidad ==5) {
+			}else if (mantenibilidad.mantenibilidad ==5) {
 				certificadoFinal = 1;
 			}
-		}else if (adecuacionFun == 2) {
-			if(mantenibilidad==1) {
+		}else if (adecuacionFun.adecuacionFun == 2) {
+			if(mantenibilidad.mantenibilidad==1) {
 				certificadoFinal = 1;
-			}else if (mantenibilidad ==2) {
+			}else if (mantenibilidad.mantenibilidad ==2) {
 				certificadoFinal = 2;
-			}else if (mantenibilidad ==3) {
+			}else if (mantenibilidad.mantenibilidad ==3) {
 				certificadoFinal = 2;
-			}else if (mantenibilidad ==4) {
+			}else if (mantenibilidad.mantenibilidad ==4) {
 				certificadoFinal = 2;
-			}else if (mantenibilidad ==5) {
+			}else if (mantenibilidad.mantenibilidad ==5) {
 				certificadoFinal = 2;
 			}
-		}else if (adecuacionFun ==3) {
-			if(mantenibilidad ==1) {
+		}else if (adecuacionFun.adecuacionFun ==3) {
+			if(mantenibilidad.mantenibilidad ==1) {
 				certificadoFinal =2;
-			}else if (mantenibilidad ==2) {
+			}else if (mantenibilidad.mantenibilidad ==2) {
 				certificadoFinal = 2;
-			}else if (mantenibilidad ==3) {
+			}else if (mantenibilidad.mantenibilidad ==3) {
 				certificadoFinal = 3;
-			}else if (mantenibilidad ==4) {
+			}else if (mantenibilidad.mantenibilidad ==4) {
 				certificadoFinal = 3;
-			}else if (mantenibilidad ==5) {
+			}else if (mantenibilidad.mantenibilidad ==5) {
 				certificadoFinal = 3;
 			}
-		}else if (adecuacionFun == 4) {
-			if (mantenibilidad ==1) {
+		}else if (adecuacionFun.adecuacionFun == 4) {
+			if (mantenibilidad.mantenibilidad ==1) {
 				certificadoFinal =3;
-			}else if (mantenibilidad ==2) {
+			}else if (mantenibilidad.mantenibilidad ==2) {
 				certificadoFinal = 3;
-			}else if (mantenibilidad ==3) {
+			}else if (mantenibilidad.mantenibilidad ==3) {
 				certificadoFinal = 3;
-			}else if (mantenibilidad ==4) {
+			}else if (mantenibilidad.mantenibilidad ==4) {
 				certificadoFinal = 3;
-			}else if (mantenibilidad ==5) {
+			}else if (mantenibilidad.mantenibilidad ==5) {
 				certificadoFinal = 4;
 			}
-		}else if(adecuacionFun==5) {
-			if (mantenibilidad ==1) {
+		}else if(adecuacionFun.adecuacionFun==5) {
+			if (mantenibilidad.mantenibilidad ==1) {
 				certificadoFinal = 3;
-			}else if (mantenibilidad ==2) {
+			}else if (mantenibilidad.mantenibilidad ==2) {
 				certificadoFinal = 3;
-			}else if (mantenibilidad ==3) {
+			}else if (mantenibilidad.mantenibilidad ==3) {
 				certificadoFinal = 4;
-			}else if (mantenibilidad ==4) {
+			}else if (mantenibilidad.mantenibilidad ==4) {
 				certificadoFinal = 4;
-			}else if (mantenibilidad ==5) {
+			}else if (mantenibilidad.mantenibilidad ==5) {
 				certificadoFinal = 5;
 			}
 		}
